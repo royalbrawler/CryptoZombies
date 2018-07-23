d424e19bbd84de7bd134c2b9351820019a2304a9
pipboy <farciebe@gmail.com>
2018-07-23 23:22:46 +0300

    functions, arrays, state variables...

--- Contract.sol
+++ Contract.sol
@@ -3,5 +3,24 @@ pragma solidity ^0.4.19;
 
 //2. Create contract here
 contract ZombieFactory {
+    // This will be stored permanently in the blockchain
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits; // equal to 10^16
+    
+    struct Zombie {
+        string name;
+        uint dna;
+    }
 
+    Zombie[] public zombies;
+
+    function _createZombie(string _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+    // view - it's only viewing the data but not modifying it
+    // pure - not even accessing any data in the app
+    function _generateRandomDna(string _str) private view returns (uint) {
+        
+    }
 }
