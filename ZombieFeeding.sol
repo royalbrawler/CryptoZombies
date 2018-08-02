cf850656b5387ce99191ff01305ff17cf707eb31
pipboy <farciebe@gmail.com>
2018-08-02 19:59:07 +0300

    storage n memory

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -4,4 +4,12 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        // storage - written permanently to the blockchain
+        // memory - will disappear when the function call ends
+        Zombie storage myZombie = zombies[_zombieId];
+
+    }
+    
 }
